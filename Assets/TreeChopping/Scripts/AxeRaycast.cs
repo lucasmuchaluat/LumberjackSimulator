b8eee31054a898dab4d6388ae50edb885878404f
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2021-05-07 14:38:27 -0300

    tree chopping almost done

---
+++ Assets/TreeChopping/Scripts/AxeRaycast.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeRaycast : MonoBehaviour
+{
+    //Variables
+    public GameObject axe;
+    private bool isEquiped = false;
+
+    private void Update()
+    {
+        if(!axe.activeSelf && Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            isEquiped = true;
+            axe.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            isEquiped = false;
+            axe.SetActive(false);
+        }
+
+        //Raycast
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        //Origin, Direction, RaycastHit, Length
+        if(Physics.Raycast(transform.position, fwd, out hit, 20))
+        {
+            if(hit.collider.tag == "tree" && Input.GetMouseButtonDown(0) && isEquiped == true)
+            {
+                Tree treeScript = hit.collider.gameObject.GetComponent<Tree>();
+                Debug.Log("Health: " + treeScript.treeHealth);
+                treeScript.treeHealth--;
+            }
+        }
+    }
+}
---
+++ Assets/TreeChopping/Scripts/Tree.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    //Variables
+    GameObject thisTree;
+    public int treeHealth = 5;
+    private bool isFallen = false;
+    private Color alphaColor;
+    private float timeToFade = 1.0f;
+
+    private void Start()
+    {
+        thisTree = transform.parent.gameObject;
+        // alphaColor = thisTree.AddComponent<Renderer>().material.color;
+        // alphaColor.a = 0;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(treeHealth <= 0 && isFallen == false)
+        {
+            Rigidbody rb = thisTree.AddComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.AddForce(Vector3.forward*3, ForceMode.Impulse);
+            StartCoroutine(destroyTree());
+            // thisTree.GetComponent<Renderer>().material.color = Color.Lerp(thisTree.GetComponent<Renderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
+            isFallen = true;
+        }
+    }
+
+    private IEnumerator destroyTree()
+    {
+        yield return new WaitForSeconds(7);
+        Destroy(thisTree);
+    }
+}
