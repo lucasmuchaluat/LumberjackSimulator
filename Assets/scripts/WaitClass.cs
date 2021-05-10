83e45573279365be143bcd6ebe1ad644eea49605
Luiz Vitor <luvi@germanos.ws>
2021-05-10 00:14:56 -0300

    Intro

---
+++ Assets/scripts/WaitClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WaitClass : MonoBehaviour
+{
+
+    public float wait = 20f;
+
+    void Start()
+    {
+        StartCoroutine(WaitIntro());
+    }
+
+    IEnumerator WaitIntro()
+    {
+        yield return new WaitForSeconds(wait);
+
+        SceneManager.LoadScene(1);
+    }
+
+}
